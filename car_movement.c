104ec867ae60915176a2cbf03017112b3ab66169
Alexander Sanabria <danielsanabria496@gmail.com>
2025-10-19 23:23:32 -0500

    Add more car movements

--- car_movement.c
+++ car_movement.c
@@ -40,6 +40,36 @@ void Move_ForwardRight(void) {
 }
 
 void Move_ForwardLeft(void) {
+    Wheel_Stop(WHEEL_L2);
+    Wheel_Stop(WHEEL_R2);
+    Wheel_MoveForward(WHEEL_L1);
+    Wheel_MoveForward(WHEEL_R1);
 }
 
+void Move_Left(void) {
+    Wheel_Forward(WHEEL_L2);
+    Wheel_Forward(WHEEL_L1);
+    Wheel_Reverse(WEHEEL_R2);
+    Wheel_Reverse(WEHEEL_R1);
+}
 
+void Move_Right(void) {
+    Wheel_Reverse(WHEEL_L2);
+    Wheel_Reverse(WHEEL_L1);
+    Wheel_Forward(WEHEEL_R2);
+    Wheel_Forward(WEHEEL_R1);
+}
+
+void Move_ReverseRight(void) {
+    Wheel_Reverse(WHEEL_L2);
+    Wheel_Reverse(WHEEL_R2);
+    Wheel_Stop(WHEEL_L1);
+    Wheel_Stop(WHEEL_R1);
+}
+
+void Move_ReverseLeft(void) {
+    Wheel_Stop(WHEEL_L2);
+    Wheel_Stop(WHEEL_R2);
+    Wheel_Reverse(WHEEL_L1);
+    Wheel_Reverse(WHEEL_R1);
+}
